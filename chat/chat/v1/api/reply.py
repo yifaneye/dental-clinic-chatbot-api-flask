afcccc4c55cec632eae4ce96d062c5755063923d
Yifan Ai <ayfallen@hotmail.com>
2020-11-16 00:49:18 +1100

    Implement reply to getDentists

--- chat/chat/v1/api/reply.py
+++ chat/chat/v1/api/reply.py
@@ -39,50 +39,17 @@ class Reply:
             return self.get_default_reply()
 
 
-class WeatherReply(Reply):
-    """A reply to user's request on weather information"""
+class DentistsReply(Reply):
+    """A reply to user's request on all available dentists"""
 
     entityName = __qualname__
 
-    def __init__(self, location):
+    def __init__(self):
         super().__init__()
-        self.location = location
-        from chat.chat.v1.api.secrets import OPEN_WEATHER_MAP_API_KEY
-        self.url = f'https://api.openweathermap.org/data/2.5/weather?q={location}&appid={OPEN_WEATHER_MAP_API_KEY}'
-
-    @staticmethod
-    def convert_kelvin_to_celsius(temperatureInKelvin):
-        """Return the temperature in Celsius of it given it Kelvin."""
-        DIFFERENCE = 273.15
-        return temperatureInKelvin - DIFFERENCE
+        self.url = f'http://0.0.0.0:8082/api/dentist/'
 
     def process_api_json_response(self):
         jsonResponse = self.get_api_json_response()
-        weather = jsonResponse['weather'][0]['description']
-        temperature = int(self.convert_kelvin_to_celsius(jsonResponse['main']['temp']))
-        return f"We have {weather} in {self.location}, the temperature is {temperature}'C."
-
-
-class RestaurantsReply(Reply):
-    """A reply to user's request on restaurants information"""
-
-    entityName = __qualname__
-
-    def __init__(self, term, location):
-        super().__init__()
-        self.term = term
-        self.location = location
-        self.url = f'https://api.yelp.com/v3/businesses/search?term={term}&location={location}'
-        from chat.chat.v1.api.secrets import YELP_API_KEY
-        self.headers = {'Authorization': YELP_API_KEY}
-
-    def process_api_json_response(self):
-        jsonResponse = self.get_api_json_response()
-        nRestaurants = len(jsonResponse['businesses'])
-        return {
-            "message": f"We have these {nRestaurants} {self.term} in {self.location}",
-            "restaurants": [
-                {"name": business["name"], "phone": business["phone"]}
-                for business in jsonResponse['businesses']
-            ]
-        }
+        dentists = [dentist['name'] for dentist in jsonResponse]
+        dentistsString = '%s' % ', '.join(map(str, dentists))
+        return f'We have {dentistsString}.'
--- chat/chat/v1/api/wit_ai.py
+++ chat/chat/v1/api/wit_ai.py
@@ -1,27 +1,22 @@
 from http import HTTPStatus
 import requests
 
-from .reply import WeatherReply, RestaurantsReply
-from .secrets import WIT_TOKEN
+from .reply import *
+from .secrets import *
 
 DEFAULT_REPLY = "Sorry, I don't understand"
 
 
 def get_reply(message):
 	"""Return the reply of a given message after processing."""
-	url = f'https://api.wit.ai/message?v=20201026&q={message}'
+	url = f'https://api.wit.ai/message?v={WIT_DATETIME}&q={message}'
 	headers = {'Authorization': WIT_TOKEN}
 	response = requests.get(url, headers=headers)
 	if response.status_code != HTTPStatus.OK:
 		raise response.raise_for_status()
 	jsonResponse = response.json()
 	try:
-		if jsonResponse['intents'][0]['name'] == "GetWeatherInformation":
-			location = jsonResponse['entities']['wit$location:location'][0]['resolved']['values'][0]['name']
-			return WeatherReply(location).get_reply()
-		elif jsonResponse['intents'][0]['name'] == "FindRestaurant":
-			location = jsonResponse['entities']['wit$location:location'][0]['resolved']['values'][0]['name']
-			query = jsonResponse['entities']['wit$local_search_query:local_search_query'][0]['value']
-			return RestaurantsReply(query, location).get_reply()
+		if jsonResponse['intents'][0]['name'] == "getDentists":
+			return DentistsReply().get_reply()
 	except:
 		return DEFAULT_REPLY
