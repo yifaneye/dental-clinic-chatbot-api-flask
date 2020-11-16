5c6eaa7e73ac0000f1aee8c27850dcbd4441ee38
Yifan Ai <ayfallen@hotmail.com>
2020-11-16 14:53:20 +1100

    Add response to updateTimeslot for basic use cases

--- chat/chat/v1/api/reply.py
+++ chat/chat/v1/api/reply.py
@@ -94,3 +94,52 @@ class DentistAvailableTimeslotReply(Reply):
         timeslots = [timeslot['startTime'] for timeslot in jsonResponse]
         timeslotsString = list_to_string(timeslots)
         return f'Dentist {self.name} is available today for 1-hour timeslots those start on {timeslotsString} o\'clock.'
+
+
+class UpdateTimeslotReply(Reply):
+    """A reply to user's request on updating a timeslot"""
+
+    entityName = __qualname__
+
+    def __init__(self, name, action, startTime):
+        super().__init__()
+        self.name = name
+        self.action = action
+        self.startTime = startTime
+        today = date.today().strftime("%Y-%m-%d")
+        self.url = f'http://0.0.0.0:8081/api/timeslot/?dentist={name}&date={today}&startTime={startTime}'
+        self.urlBackup = f'http://0.0.0.0:8081/api/timeslot/?dentist={name}&date={today}'
+
+    @staticmethod
+    def get_api_json_response_alternative(url):
+        """Returns the JSON response by making an API call"""
+        response = requests.get(url)
+        return response.json()
+
+    @staticmethod
+    def patch_api_json_response_alternative(url, data):
+        """Returns the JSON response by making an API call"""
+        response = requests.patch(url, data=data)
+        if response.status_code != 200:
+            return 'failed to'
+        return 'succeed to'
+
+    def process_api_json_response(self):
+        jsonResponse = self.get_api_json_response()
+        if len(jsonResponse) == 0:
+            jsonResponseBackup = self.get_api_json_response_alternative(self.urlBackup)
+            standardReply = f'We do not have dentist {self.name} working at {self.startTime} today.'
+            if self.action == 'reserve' and len(jsonResponseBackup) >= 1:
+                timeSlotBackup = jsonResponseBackup[0]
+                return f'{standardReply} I suggest with Dentist {self.name}\'s 1-hour timeslot that start on {timeSlotBackup["startTime"]} o\'clock today.'
+            return standardReply
+        timeslot = jsonResponse[0]
+        url = f'http://0.0.0.0:8081/api/timeslot/{timeslot["id"]}/'
+        if self.action == 'cancel':
+            timeslot['status'] = 'available'
+            response = self.patch_api_json_response_alternative(url, data=timeslot)
+            return f'I {response} cancel the reservation with dentist {self.name} at {self.startTime} for you.'
+        elif self.action == 'reserve':
+            timeslot['status'] = 'reserved'
+            response = self.patch_api_json_response_alternative(url, data=timeslot)
+            return f'I {response} reserve the reservation with dentist {self.name} at {self.startTime} for you.'
--- chat/chat/v1/api/wit_ai.py
+++ chat/chat/v1/api/wit_ai.py
@@ -4,27 +4,34 @@ import requests
 from .reply import *
 from .secrets import *
 
-DEFAULT_REPLY = "Sorry, I don't understand"
-ERROR_REPLY = "Sorry, I experienced error."
+DEFAULT_REPLY = "Sorry, I understand your intent but I am not programmed to respond to this intent."
+ERROR_REPLY = "Sorry, I can not understand your intent from my training."
 
 
 def get_reply(message):
-	"""Return the reply of a given message after processing."""
-	url = f'https://api.wit.ai/message?v={WIT_DATETIME}&q={message}'
-	headers = {'Authorization': WIT_TOKEN}
-	response = requests.get(url, headers=headers)
-	if response.status_code != HTTPStatus.OK:
-		raise response.raise_for_status()
-	jsonResponse = response.json()
-	try:
-		if jsonResponse['intents'][0]['name'] == "getDentists":
-			return DentistsReply().get_reply()
-		elif jsonResponse['intents'][0]['name'] == "getDentistInformation":
-			name = jsonResponse['entities']['dentist:dentist'][0]['body']
-			return DentistInformationReply(name).get_reply()
-		elif jsonResponse['intents'][0]['name'] == "getDentistAvailableTimeslots":
-			name = jsonResponse['entities']['dentist:dentist'][0]['body']
-			return DentistAvailableTimeslotReply(name).get_reply()
-		return DEFAULT_REPLY
-	except:
-		return ERROR_REPLY
+    """Return the reply of a given message after processing."""
+    url = f'https://api.wit.ai/message?v={WIT_DATETIME}&q={message}'
+    headers = {'Authorization': WIT_TOKEN}
+    response = requests.get(url, headers=headers)
+    if response.status_code != HTTPStatus.OK:
+        raise response.raise_for_status()
+    jsonResponse = response.json()
+    try:
+        if jsonResponse['intents'][0]['name'] == "getDentists":
+            return DentistsReply().get_reply()
+
+        elif jsonResponse['intents'][0]['name'] == "getDentistInformation":
+            name = jsonResponse['entities']['dentist:dentist'][0]['body']
+            return DentistInformationReply(name).get_reply()
+        elif jsonResponse['intents'][0]['name'] == "getDentistAvailableTimeslots":
+            name = jsonResponse['entities']['dentist:dentist'][0]['body']
+            return DentistAvailableTimeslotReply(name).get_reply()
+        elif jsonResponse['intents'][0]['name'] == "updateTimeslot":
+            name = jsonResponse['entities']['dentist:dentist'][0]['body'] or ''
+            action = jsonResponse['entities']['action:action'][0]['body'] or ''
+            # Get hour 13 -> 13, 09 -> 9
+            startTime = jsonResponse['entities']['wit$datetime:datetime'][0]['value'][11:13].lstrip('0') or ''
+            return UpdateTimeslotReply(name, action, startTime).get_reply()
+        return DEFAULT_REPLY
+    except:
+        return ERROR_REPLY
